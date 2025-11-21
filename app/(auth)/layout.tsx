0860e527b885cb49ac71376e0108965e770880d3
Justin <justinaidiatama2017@gmail.com>
2025-11-21 16:44:47 +0700

    Update new ffiles and features

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='bg-gray'>
+        <div className='flex flex-col items-center justify-between h-screen px-6 py-8 mx-auto '>
+            <div className="w-full bg-white rounded-lg shadow mt-0 max-w-md">
+                {children}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AuthLayout
