f886b93f5402c20cc46dfe12ed55f2d730d7ae15
Justin <justinaidiatama2017@gmail.com>
2025-12-04 19:09:35 +0700

    update lagi

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,8 @@ import path from "path";
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname),
   outputFileTracingIncludes: {
-    "/api/**/*": ["./lib/generated/prisma/**/*"],
-    "/api/**/**/**": ["./lib/generated/prisma/**/*"],
-    "/*": ["./lib/generated/prisma/**/*"],
+    "/api/**/*": ["./lib/generated/prisma/*"],
+    "/*": ["./lib/generated/prisma/*"],
   },
 };
 
